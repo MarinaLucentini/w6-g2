64aa06fc89aba9c8395b30af201b05eca67f8b15
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-04-09 16:53:21 +0200

    commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const nameUser = document.getElementById("inputName");
+const textUser = document.getElementById("inputNameText");
+const btnSubmit = document.getElementById("btnSubmit");
+const btnDelete = document.getElementById("btnDelete");
+const form = document.querySelector("form");
+const NAME_USER_INPUT = "name-user";
+const nameSavedArray = [];
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nameSaved = nameUser.value;
+  nameSavedArray.push(nameSaved);
+  localStorage.setItem(NAME_USER_INPUT, JSON.stringify(nameSavedArray));
+
+  console.log("salvato");
+  const textUserName = localStorage.getItem("name-user");
+  console.log(textUserName);
+
+  textUser.innerText = "Il nome salvato è " + nameSaved;
+  form.reset();
+  console.log(nameSavedArray);
+  const existName = JSON.parse(textUserName);
+  btnDelete.addEventListener("click", () => {
+    console.log(existName);
+    console.log(nameSavedArray);
+    if (existName) {
+      textUser.innerText = "Il nome è stato correttamente eliminato";
+      existName.pop();
+      nameSavedArray.pop();
+      localStorage.setItem("name-user", JSON.stringify(existName));
+    } else {
+      textUser.innerText = "Non esiste nessun nome da eliminare";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", timer);
+function timer() {
+  let count = sessionStorage.getItem("timerCount");
+  setInterval(() => {
+    count++;
+    document.getElementById("counter").innerText =
+      "sono passati " + count + " secondi";
+    sessionStorage.setItem("timerCount", count);
+  }, 1000);
+}
